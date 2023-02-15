a5c723f6193b60bacad5e31eb6a1ecff999c4f15
Yuri Neves <yurineves92@gmail.com>
2023-02-15 20:57:37 -0300

    update models

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function profile()
+    {
+        
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+}
